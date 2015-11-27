170df5fd2589605d7a445831b20263d1a2f10ea7
Sam Corin <samcorin@gmail.com>
2015-11-27 17:45:05 +0900

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('App', []);
+
+app.controller('mainCtrl', function($scope, $http) {
+    $scope.res = [];
+    
+    $scope.hotStoriesAPI = "http://www.freecodecamp.com/news/hot";
+
+    $http.get($scope.hotStoriesAPI)
+    	.success(function(hotStories) {
+        	hotStories.forEach(function(hotStory) {
+	            $scope.headline = hotStory.headline;
+	            $scope.storyLink = hotStory.link;
+	            $scope.discussionLink = hotStory.storyLink;
+	            $scope.authorPicture = hotStory.author.picture;
+	            $scope.authorName = hotStory.author.username;
+	            if ($scope.authorName === undefined) {
+	                $scope.authorName = "unknown"
+	            }
+	            $scope.upVotes = hotStory.rank;
+	            $scope.timePosted = new Date(hotStory.timePosted).toGMTString().slice(0, -13);
+	            $scope.timePosted = "Posted on: " + $scope.timePosted;
+	            $scope.res.push({
+	                headline: $scope.headline,
+	                storyLink: $scope.storyLink,
+	                discussionLink: $scope.discussionLink,
+	                authorPicture: hotStory.author.picture,
+	                authorName: $scope.authorName,
+	                upVotes: hotStory.rank,
+	                timePosted: new Date(hotStory.timePosted).toGMTString().slice(0, -13),
+	                timePosted: $scope.timePosted
+	            })
+        })
+        console.log($scope.res)
+    })
+})
